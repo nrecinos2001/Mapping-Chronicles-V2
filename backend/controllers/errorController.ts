74b0b296c7a43fcc59f5d3771a3bb1b84e429305
Nestor Recinos <00083120@UCA.EDU.SV>
2024-06-17 15:29:38 -0600

    :sparkles: Rename .js file to .ts and :label: Update types for error controllers function

--- backend/controllers/errorController.ts
+++ backend/controllers/errorController.ts
@@ -1,24 +1,27 @@
+import { AppError } from "@Utils/appError";
+import { Response } from "express";
+import { ICastErrorDb, IControllerError, IDuplicateFieldsDb, IValidationErrorDb } from "types";
+
 /*
   Copyright 2023 Mapping Chronicles
   Use of this source code is governed by an MIT-style
   license that can be found in the LICENSE file or at
   https://opensource.org/licenses/MIT.
 */
-const AppError = require('../utils/appError');
 
-const handleCastErrorDB = (err) => {
+const handleCastErrorDB = (err: ICastErrorDb) => {
     const message = `Invalid ${err.path} is ${err.value}`;
     return new AppError(message, 400);
 };
 
-const handleValidationErrorDB = (err) => {
+const handleValidationErrorDB = (err: IValidationErrorDb) => {
     const errors = Object.values(err.errors).map((el) => el.message);
     const message = `Invalid input data. ${errors.join('. ')}`;
     return new AppError(message, 400);
 };
 
 // to be used for duplicate user emails
-const handleDuplicateFieldsDB = (err) => {
+const handleDuplicateFieldsDB = (err: IDuplicateFieldsDb) => {
     const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
     const message = `${value} already exists`;
     return new AppError(message, 400);
@@ -28,7 +31,7 @@ const handleJWTError = () => new AppError('Invalid token. Please log in again', 
 
 const handleJWExpiredError = () => new AppError('Token has expired. please log in again', 401);
 
-const sendErrorDev = (err, res) => {
+const sendErrorDev = (err: AppError, res: Response) => {
     res.status(err.statusCode).json({
         status: err.status,
         error: err,
@@ -37,7 +40,7 @@ const sendErrorDev = (err, res) => {
     });
 };
 
-const sendErrorProd = (err, res) => {
+const sendErrorProd = (err: IControllerError, res: Response) => {
     // if the error is operational (no internet conection for example), display it to the user on production
     if (err.isOperational) {
         res.status(err.statusCode).json({
@@ -53,14 +56,14 @@ const sendErrorProd = (err, res) => {
     }
 };
 
-module.exports = (err, req, res, next) => {
+export const errorHandler = (err: AppError, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        let error = Object.assign(err); // create a hard copy of the original error object to avoid its mutation. {...err} destructuring was not working properly, switched to Object.assign
+        let error = Object.assign(err) as IControllerError; // create a hard copy of the original error object to avoid its mutation. {...err} destructuring was not working properly, switched to Object.assign
 
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
