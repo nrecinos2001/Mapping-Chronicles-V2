6f00535e45cb58989b459ccddb7de34d713b1fb6
Nestor Recinos <00083120@UCA.EDU.SV>
2024-06-17 21:49:38 -0600

    :recycle: Refactor mailer file

--- backend/utils/email.ts
+++ backend/utils/email.ts
@@ -4,21 +4,27 @@
   license that can be found in the LICENSE file or at
   https://opensource.org/licenses/MIT.
 */
-import nodemailer from 'nodemailer';
+import { emailHost, emailPassword, emailPort, emailSender, emailUsername } from '@Constants/mailer';
+import { EmailOptions } from 'types';
 
-export const sendEmail = async (options) => {
+import nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+export const sendEmail = async (options: EmailOptions) => {
     // 1) Create transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+    const transporterOptions: SMTPTransport.Options = {
+        host: emailHost,
+        port: emailPort,
         auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
+            user: emailUsername,
+            pass: emailPassword,
         },
-    });
+    };
+
+    const transporter = nodemailer.createTransport(transporterOptions);
     // 2) Define email options
     const mailOptions = {
-        from: 'Edwin Gomez <edwin@chronicles.com>',
+        from: emailSender,
         to: options.email,
         subject: options.subject,
         text: options.message,
